b62d0adb6574a62766b5da19c2ecb9bd831fb193
kamkara <lnclass.kamkara@gmail.com>
2022-09-10 20:25:41 +0000

    Add Daisyui

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,35 @@
 module ApplicationHelper
+
+    #Onglet Title
+    def title
+        base_title = "Lnclass Education"
+        if @title.nil?
+        base_title
+        else
+        "#{@title} • #{base_title}"
+        end
+    end
+    def app_name
+      "Lnclass Education"
+    end
+
+    def language
+      "fr"
+    end
+    #site description
+    def description
+        "Etudier avec facilité."
+    end
+    #Site Keys worlds
+    def keywords
+    "Tle, Bac, Terminale A1 Terminale A2,2nd C ..."
+    end
+    #Theme color
+    def theme_color
+      "#020408"      
+    end
+    
+    def tileColor
+      "#020408"
+    end
 end

74f1c8b07beaab210fc403add9f0f2e1e55e30ef
kamkara <lnclass.kamkara@gmail.com>
2022-09-10 21:24:22 +0000

    Add Dashboard config

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,13 @@
 class DashboardController < ApplicationController
+    before_action :authenticate_user!
+  
   def index
   end
 
   def home
+    @LevelList = Level.all_ordered
+    @CityList = Citytown.all_ordered
+    @MaterialList = Material.all_ordered
+    @SchoolList = School.all_ordered
   end
 end

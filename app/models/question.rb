2bed4c773cd3ea66bde171e3814aa6b01d948ef3
kamkara <lnclass.kamkara@gmail.com>
2022-09-15 09:38:43 +0000

    app/models/question: Add

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,10 @@
 class Question < ApplicationRecord
   belongs_to :user
   belongs_to :course
-
+  has_many :anwsers, class_name: "anwser", foreign_key: "question_id", dependent: :destroy
+  #setup validation and uniqueness
+  validates :title, presence: true, uniqueness: { scope: :course_id }
+  
   #Slugged concern
   include ItemsSlugged
 

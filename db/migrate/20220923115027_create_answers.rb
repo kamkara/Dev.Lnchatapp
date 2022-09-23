0dc7e924415aafe057f0ef8bfec18c5ccab05354
kamkara <lnclass.kamkara@gmail.com>
2022-09-23 12:13:51 +0000

    Add Answer

---
+++ db/migrate/20220923115027_create_answers.rb
@@ -0,0 +1,11 @@
+class CreateAnswers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :answers, id: :uuid do |t|
+      t.boolean :correct_answer, null: false
+      t.belongs_to :user, null: false, foreign_key: true, type: :uuid
+      t.belongs_to :question, null: false, foreign_key: true, type: :uuid
+
+      t.timestamps
+    end
+  end
+end

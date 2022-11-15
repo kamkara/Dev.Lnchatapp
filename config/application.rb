68f1805fceeec1777cbb836a08ab04570c14da81
kamkara <lnclass.kamkara@gmail.com>
2022-11-15 11:55:05 +0000

    Add Upgrade rails 7.0.3 to 7.0.4

--- config/application.rb
+++ config/application.rb
@@ -18,8 +18,5 @@ module Lnchatapp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-
-    #### Custom Errors page
-    config.exceptions_app = self.routes
   end
 end
